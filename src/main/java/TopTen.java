51fdc9390db9186ddb7a94e6a798971232d09010
malavelika <senka.music@gmail.com>
2017-07-14 17:14:26 +0200

    SenkaA: Remove script tags

--- src/main/java/TopTen.java
+++ src/main/java/TopTen.java
@@ -49,10 +49,30 @@ public class TopTen  implements TopTenInterface {
         int endIndex = html.indexOf("</body>", startIndex);
         return html.substring(startIndex, endIndex);
     }
-
+	
+	private String removeScriptTag (String body) {
+        StringBuilder sb = new StringBuilder();
+        int firstI=-1;
+        int lastI =-1;
+        int i=0;
+		while (i<body.length()){
+				firstI = body.indexOf("<script", i); 
+				lastI = body.indexOf("/script>", firstI)+"/script>".length(); 
+				if (firstI<0) {
+					sb.append(body.substring(i)); 
+					i=body.length();
+					}
+				else {
+					sb.append(body.substring(i, firstI)); 
+					i = lastI;
+					}
+			}
+        return sb.toString();
+    }
+	
     public String removeHtmlTags(String body) {
         StringBuilder sb = new StringBuilder();
-
+		body = removeScriptTag (body);
         int start = -1;
         //int end = -1;
         for(int i=0; i < body.length(); i++)
@@ -73,6 +93,7 @@ public class TopTen  implements TopTenInterface {
             sb.append(c);
 
         }
+
         return sb.toString();
     }
 
